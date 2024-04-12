59d1ca545890f29717506a29e3ce7584b06b9618
Sergei Budygin <145099561+Svbudygin@users.noreply.github.com>
2024-04-12 09:33:18 +0300

    Update main.cpp

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include "fft/furie.h"
 
+// int main()
+// {
+// //    std::string filename = "../songs/GSPD-RUSSIA_HAS_3_PATHS.mp3";
+//     std::string filename = "D:/studying/prog/furie_cpp/furie_cpp/songs/song.wav";
+//     std::cout << filename << std::endl;
+
+//     processAudioFile(filename);
+
+//     return 0;
+// }
 int main()
 {
 //    std::string filename = "../songs/GSPD-RUSSIA_HAS_3_PATHS.mp3";
     std::string filename = "D:/studying/prog/furie_cpp/furie_cpp/songs/song.wav";
     std::cout << filename << std::endl;
-
-    processAudioFile(filename);
+    std::vector<float> localMax;
+    localMax = processAudioFile(filename);
 
     return 0;
 }
